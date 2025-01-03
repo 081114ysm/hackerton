b6f5b6b592094cf3ced77ec5efcb538b900e6088
Knhye <knahye311@gmail.com>
2025-01-03 09:22:54 +0900

    Update profile

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Profile = () => {
   return (
@@ -13,7 +14,17 @@ const Profile = () => {
           height: "100px",
         }}
       />
-      <div style={{ color: "whitesmoke" }}>User</div>
+      <Link
+        to="/mypage"
+        style={{
+          color: "whitesmoke",
+          cursor: "pointer",
+          textDecoration: "none",
+          display: "block",
+        }}
+      >
+        User
+      </Link>
     </div>
   );
 };
